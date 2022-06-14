997ae73e8b981008f3e46edb78bb7ec279e3ce77
McLaughlin, Jamie W <JMcLaughlin@jjkeller.com>
2022-06-14 16:06:37 -0400

    added input validation

--- Notes/Notes.WebApi/wwwroot/Scripts/notes.jsx
+++ Notes/Notes.WebApi/wwwroot/Scripts/notes.jsx
@@ -1,136 +1,150 @@
 ﻿class Notes extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null, isLoaded: false, backend: "local", newNote: { "Id": 0, "NoteText": "" }, notes: props.notes || []
-    };
-  }
-
-  componentDidMount() {
-    fetch("/api/note")
-        .then(res => {
-            //throw "hello world";
-            return res.json();
-        })
-      .then(
-          (result) => {
-          this.setState({
-            isLoaded: true,
-            notes: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  saveNote = (note) => {
-    fetch('/api/note', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(note)
-    })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          var newNotes = [...this.state.notes, result];
-          this.setState({ newNote: { "Id": 0, "NoteText": "" }, notes: newNotes });
-        });
-  }
-
-  deleteNote = (note) => {
-    fetch(`/api/note/${note.Id}`, {
-      method: 'DELETE',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
-    })
-  }
-
-  handleNewNoteKeyDown = (event) => {
-    // enter key
-    if (event.keyCode !== 13) {
-      return;
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null, isLoaded: false, backend: "local", newNote: { "Id": 0, "NoteText": "" }, notes: props.notes || []
+        };
     }
 
-    event.preventDefault();
+    componentDidMount() {
+        fetch("/api/note")
+            .then(res => {
+                //throw "hello world";
+                return res.json();
+            })
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        notes: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
-    this.handleSaveNote();
-  }
+    saveNote = (note) => {
+        fetch('/api/note', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(note)
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    var newNotes = [...this.state.notes, result];
+                    this.setState({ newNote: { "Id": 0, "NoteText": "" }, notes: newNotes });
+                });
+    }
 
-  handleSaveNote = () => {
-    var newNoteText = this.state.newNote.NoteText;
-    var newNote = { "Id": 0, "NoteText": newNoteText };
-    this.saveNote(newNote);
-  }
+    deleteNote = (note) => {
+        fetch(`/api/note/${note.Id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+    }
 
-  backendChange = (event) =>
-    this.setState({ backend: event.target.value });
+    handleNewNoteKeyDown = (event) => {
+        // enter key
+        if (event.keyCode !== 13) {
+            return;
+        }
 
-  handleNoteDelete = (note) => {
+        event.preventDefault();
+        this.handleSaveNote();
+    }
 
-    this.deleteNote(note);
+    handleSaveNote = () => {
+        var newNoteText = this.state.newNote.NoteText;
+        var error = this.validateNote(newNoteText);
+        if (error) {
+            this.setState({ error });
+        }
+        else {
+            var newNote = { "Id": 0, "NoteText": newNoteText };
+            this.saveNote(newNote);
+        }
+    }
 
-    var notes = this.state.notes.filter(item => item !== note);
-    this.setState({
-      notes
-    });
+    validateNote = (newNoteText) => {
+        if (!newNoteText || newNoteText == "") {
+            return "no note text!";
+        }
+        if (newNoteText.trim() == "") {
+            return "must enter text!";
+        }
+    }
 
-    this.state.newNote = { "Id": 0, "NoteText": "" };
-  };
+    backendChange = (event) =>
+        this.setState({ backend: event.target.value });
 
-  handleNoteEdit = (note) => {
-    var notes = this.state.notes.filter(item => item !== note);
-    this.setState({
-      notes, newNote: note
-    });
-  }
+    handleNoteDelete = (note) => {
 
-  handleNewNoteChange = (event) =>
-    this.setState({ newNote: { "Id": this.state.newNote.Id, "NoteText": event.target.value } });
+        this.deleteNote(note);
 
-  render() {
-    return (
-      <div className="col-md-12">
-        <div className="row">
-          <div className="col-md-3">
-            <input type="text" value={this.state.newNote.NoteText} onChange={this.handleNewNoteChange} onKeyDown={this.handleNewNoteKeyDown} ></input>
-          </div>
-          <div className="col-md-3">
-            <button className="btn btn-success btn-sm" onClick={_ => this.handleSaveNote()} >Save</ button>
-          </div>
+        var notes = this.state.notes.filter(item => item !== note);
+        this.setState({
+            notes
+        });
+
+        this.state.newNote = { "Id": 0, "NoteText": "" };
+    };
+
+    handleNoteEdit = (note) => {
+        var notes = this.state.notes.filter(item => item !== note);
+        this.setState({
+            notes, newNote: note
+        });
+    }
+
+    handleNewNoteChange = (event) =>
+        this.setState({ newNote: { "Id": this.state.newNote.Id, "NoteText": event.target.value }, "error": "" });
+
+    render() {
+        return (
+            <div className="col-md-12">
+                <div className="row">
+                    <div className="col-md-3">
+                        <input type="text" value={this.state.newNote.NoteText} onChange={this.handleNewNoteChange} onKeyDown={this.handleNewNoteKeyDown} ></input>
+                    </div>
+                    <div className="col-md-3">
+                        <button className="btn btn-success btn-sm" onClick={_ => this.handleSaveNote()} >Save</ button>
+                    </div>
+                </div>
+
+                <div className="row text-danger">
+                    {this.state.error}
+                </div>
+
+                <div>
+                    {this.state.notes.map((note, index) =>
+                        <div key={index} className="row">
+                            <div className="col-md-3">
+                                <a href={"/api/note/" + note.Id}>
+                                    {note.NoteText}
+                                </a>
+                            </div>
+                            <div className="col-md-3">
+                                <button className="btn btn-primary btn-sm" onClick={_ => this.handleNoteEdit(note)} >Edit</ button>
+                                <button className="btn btn-danger btn-sm" onClick={_ => this.handleNoteDelete(note)} >Delete</ button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
-
-            <div className="row text-danger">
-                {this.state.error}
-            </div>
-
-        <div>
-          {this.state.notes.map((note, index) =>
-            <div key={index} className="row">
-                  <div className="col-md-3">
-                      <a href={"/api/note/" + note.Id}>
-                        {note.NoteText}
-                      </a>
-              </div>
-              <div className="col-md-3">
-                <button className="btn btn-primary btn-sm" onClick={_ => this.handleNoteEdit(note)} >Edit</ button>
-                <button className="btn btn-danger btn-sm" onClick={_ => this.handleNoteDelete(note)} >Delete</ button>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
